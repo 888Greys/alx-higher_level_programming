affb336ab358ba000a764a7df80cc51f8fac5f3c
888Greys <toxicgreys001@gmail.com>
2023-08-09 17:10:57 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 9-print_last_digit.py
+"""writing this code in the midnight"""
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
