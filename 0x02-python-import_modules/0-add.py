3bdc8ed04b952f337287ee245c58aeb25adc8fdd
888Greys <toxicgreys001@gmail.com>
2023-08-11 14:30:20 -0700

    Code that prints the sum of 1 and 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
+if __name__ == "__main__":
+      """Code that prints the sum of one and two."""
     from add_0 import add
-    a = 2
-    b = 1
+
+    a = 1
+
+    b = 2
+
     print("{} + {} = {}".format(a, b, add(a, b)))
