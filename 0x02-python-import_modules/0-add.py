7f9b1a5fae5486644e9ca4bd71cd8f66f87c8131
888Greys <toxicgreys001@gmail.com>
2023-08-11 14:44:56 -0700

    Code that prints the value of 1 and 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     """The code that prints out the sum of one and three"""
     from add_0 import add
 
-    b = 2
-    a = 1
+    a = 2
+    b = 1
     print("{} + {} = {}".format(a, b, add(a, b, add(a, b)))
