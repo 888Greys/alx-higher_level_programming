bc540040b2bc864b43e3924eb7bcafbdf5163d07
888Greys <toxicgreys001@gmail.com>
2023-08-11 14:13:55 -0700

    Modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+import add_0
 
+def main():
     a = 1
-
     b = 2
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    main()
