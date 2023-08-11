dc964ebaaaf7ff63d51236b54a52e5f56ec7e727
888Greys <toxicgreys001@gmail.com>
2023-08-11 15:17:51 -0700

    Print multiple, difference, sum and quotient of 10 and 5

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,12 +2,12 @@
 
 if __name__ == "__main__":
     """Print the quotient, multiple, difference and sum of 5 and 10."""
-    from calculator_1 import sub, add, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
