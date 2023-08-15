7b08612c1bb2378071e1019d98017ce3fd509004
888Greys <toxicgreys001@gmail.com>
2023-08-15 14:09:56 -0700

    Function that replaces an element in a list of a specific position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Thise function replaces an element in the list
+# at a specific position without modifying the original list
+
+def new_in_list(my_list, idx, element):
+
+    if idx < 0 or idx > (len(my_list) - 1):
+
+        return (my_list)
+
+    copy = [x for x in my_list]
+
+    copy[idx] = element
+
+    return (copy)
