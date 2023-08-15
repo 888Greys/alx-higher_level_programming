65c279d92be1f281d1d88c0ef0b04281e942a337
888Greys <toxicgreys001@gmail.com>
2023-08-15 09:31:00 -0700

    element at an index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
-#!/bin/bash/python3
-def find_index(number, my_list):
-    if number in my_list:
-        index = my_list.index(number)
+#!/usr/bin/python3
+# 1-element_at.py
 
-        if index < 0 or index >= len(my_list):
-            print("None")
-        else:
-            print(f"The number {number} is at index {index}")
-    else:
-        print(f"The number {number} is not in the list")
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
+
