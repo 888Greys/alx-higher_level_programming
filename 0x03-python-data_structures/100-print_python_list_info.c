5a71490a0815b83a662e62fbf8adcfcc98deb23b
888Greys <toxicgreys001@gmail.com>
2023-08-15 16:52:21 -0700

    c function that prints some vasic c function about lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,6 @@
 #include <Python.h>
-
 /**
- * print_python_list_info - Prints basic information about Python lists.
+ * print_python_list_info - Prints basic info about Python lists.
  * @p: A PyObject list.
  */
 void print_python_list_info(PyObject *p)
@@ -10,8 +9,8 @@ int size, alloc, i;
 PyObject *obj;
 size = Py_SIZE(p);
 alloc = ((PyListObject *)p)->allocated;
-printf("[*] Size of the Python List is = %d\n", size);
-printf("[*] Allocated at = %d\n", alloc);
+printf("[*] Size of the Python List = %d\n", size);
+printf("[*] Allocated = %d\n", alloc);
 for (i = 0; i < size; i++)
 {
 printf("Element %d: ", i);
