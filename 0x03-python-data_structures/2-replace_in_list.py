660279c6bbd37acaa56bac9e0e94f4ba463089a2
888Greys <toxicgreys001@gmail.com>
2023-08-15 10:52:01 -0700

    Replacing elements in a list at a specific position

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/bin/bash/python3
+"""
+Replacing an element
+"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx >= and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
