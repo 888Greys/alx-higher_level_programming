82a6de3c33a9164c793c9eedb2685f41b71944a1
888Greys <toxicgreys001@gmail.com>
2023-08-18 16:50:19 -0700

    A function that adds or replaces a value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,7 +5,4 @@ This function replaces or adds a value to a dictionary
 
 
 def update_dictionary(a_dictionary, key, value):
-    if a_key in a_dictionary:
-        a_dictionary[a_key] = value
-    else:
-        a_dictionary[a_key] = value
+    a_dictionary[key] = value
