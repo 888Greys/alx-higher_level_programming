aad74bd5059147c36386fdd72c2d161ea0d49e40
888Greys <toxicgreys001@gmail.com>
2023-08-18 16:48:41 -0700

    A function that adds or replaces a value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This function replaces or adds a value to a dictionary
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    if a_key in a_dictionary:
+        a_dictionary[a_key] = value
+    else:
+        a_dictionary[a_key] = value
