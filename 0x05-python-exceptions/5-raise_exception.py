d66ee5de00e57db3599fd9aaf31808671753362a
888Greys <toxicgreys001@gmail.com>
2023-08-30 01:58:06 -0700

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This Python code defines a function, `raise_exception`,
+that deliberately raises a `TypeError` exception when called.
+It serves as a simple demonstration of how
+to raise custom exceptions in Python.
+"""
+
+
+def raise_exception():
+    raise TypeError
