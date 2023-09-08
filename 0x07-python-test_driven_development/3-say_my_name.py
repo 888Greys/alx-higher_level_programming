4cb90f7cdf99992ee4bd5aa761ed9bc9d09b3276
888Greys <toxicgreys001@gmail.com>
2023-09-08 13:28:17 -0700

    say my name:

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,14 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """Print a name
 
-
+    Arguements:
+        first_name (string): The first name to print.
+        last_name (string): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
