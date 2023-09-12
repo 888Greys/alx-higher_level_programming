39fda6e52b7d70734c639904ecebb64bed7ae58a
888Greys <toxicgreys001@gmail.com>
2023-09-12 12:26:56 -0700

    repeat

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
-# class that defines a square
-
+# This module defines a Square class.
 
 class Square:
+    """
+    This class represents a square.
+
+    A square is a geometric shape with four equal sides and four equal angles.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+
     pass
