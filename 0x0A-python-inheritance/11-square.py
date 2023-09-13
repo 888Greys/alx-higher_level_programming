b050f780c7abd0257ebdd20b77492855989f6483
888Greys <toxicgreys001@gmail.com>
2023-09-13 13:29:22 -0700

    square:

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,49 +1,18 @@
 #!/usr/bin/python3
-"""
-Square - A class representing a square that inherits
-from Rectangle.
-"""
-
+""" square class """
 Rectangle = __import__('9-rectangle').Rectangle
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(Rectangle):
-    """
-    Square is a class representing a square that inherits
-    from Rectangle.
-
-    It provides methods for initializing the dimensions
-    of the square,
-    including its side length,
-    and generating a string representation.
-
-    Attributes:
-        __size (int): The side length of the square.
-    """
+    """ square """
     def __init__(self, size):
-        """
-        Initialize a Square instance with the given side length.
-
-        Args:
-            size (int): The side length of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is not greater than 0.
-        """
+        """ initialize """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
     def __str__(self):
-        """
-        Generate a string representation of the square.
-
-        Returns:
-            str: A string representation in the format
-            "[Square] size/size".
-        """
+        """ str """
         string = "[Square] " + str(self.__size) + "/" + str(self.__size)
         return string
