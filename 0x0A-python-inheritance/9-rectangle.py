2021fd9412d19f246fd816e75a96d05fbc31ecfe
888Greys <toxicgreys001@gmail.com>
2023-09-13 13:04:03 -0700

    Square

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,60 +1,13 @@
 #!/usr/bin/python3
-"""
-Rectangle - A class representing a rectangle that
-inherits from BaseGeometry.
-"""
-
-
+""" square#1  class """
+Rectangle = __import__('9-rectangle').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):
-    """
-    Rectangle is a class representing a rectangle that
-    inherits from BaseGeometry.
-
-    It provides methods for initializing the dimensions of
-    the rectangle, calculating its area,
-    and generating a string representation.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
-    def __init__(self, width, height):
-        """
-        Initialize a Rectangle instance with the given width
-        and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is not greater than 0.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Calculate and return the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Generate a string representation of the rectangle.
-
-        Returns:
-            str: A string representation in the format
-            "[Rectangle] width/height".
-        """
-        string = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
-        return string
+class Square(Rectangle):
+    """ square: its really working """
+    def __init__(self, size):
+        """ initialization """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
