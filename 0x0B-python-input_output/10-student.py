0e76f5dcddad568f080b06d8df056aa9fb92c8c6
888Greys <toxicgreys001@gmail.com>
2023-09-13 14:39:30 -0700

    Student with JSON to filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 """
-Class Student
+This module defines a Student class.
 """
 
 
 class Student:
+
     def __init__(self, first_name, last_name, age):
-        """ initialize"""
+        """
+        Initialize a Student object.
+
+        :param first_name: The first name of the student.
+        :param last_name: The last name of the student.
+        :param age: The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ return a dict of self """
-        return vars(self)
+
+        """
+        Return a dictionary representation of the
+        Student object.
+
+        :param attrs: A list of attribute names to
+        include in the dictionary (default is None).
+        :return: A dictionary representing the
+        Student object.
+        """
+        if attrs is None:
+            return vars(self)
+        return {attr: getattr(self, attr, None) for attr in attrs}
