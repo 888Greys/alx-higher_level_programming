12c3ba9b8dfd4768c069dd24baf2fb6004668e08
888Greys <toxicgreys001@gmail.com>
2023-09-21 07:43:31 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-
+""" Base class """
 import json
 import turtle
 
 
 class Base:
-    """
-    Base class for managing objects.
-
-    This class provides essential methods for object serialization,
-    deserialization, and drawing using the 'turtle' module.
-    """
-
+    """ Base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Constructor for the Base class.
-
-        Args:
-            id (int): An optional identifier for the object.
-            If not provided, it will be automatically assigned.
-        """
         if id is None:
             type(self).__nb_objects += 1
             self.id = self.__nb_objects
@@ -30,29 +17,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Convert a list of dictionaries to a JSON string.
-
-        Args:
-            list_dictionaries (list): A list of dictionaries to be converted.
-
-        Returns:
-            str: A JSON string representation of the input list of
-            dictionaries.
-        """
+        """ converts dict to json """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """
-        Save a list of objects as a JSON string representation to a file.
-
-        Args:
-            list_objs (list): A list of objects to be serialized and
-            saved to a file.
-        """
+        """ writes the JSON string representation to file """
         LO_dict = []
         if list_objs is not None:
             LO_dict = [x.to_dictionary() for x in list_objs]
@@ -61,33 +33,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """
-        Convert a JSON string representation to a list of dictionaries.
-
-        Args:
-            json_string (str): A JSON string to be converted.
-
-        Returns:
-            list: A list of dictionaries parsed from the JSON string.
-        """
+        """ returns the list of the JSON string representation """
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """
-        Create an instance of the class and return it, initializing it
-        with values from a dictionary.
-
-        Args:
-            dictionary (dict): A dictionary containing attributes to
-            initialize the object.
-
-        Returns:
-            Base: An instance of the class with attributes set from
-            the input dictionary.
-        """
+        """ create an instance of the class and return it """
         if cls.__name__ == "Rectangle":
             inst = cls(1, 1)
         elif cls.__name__ == "Square":
@@ -99,13 +52,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """
-        Load a list of instances from a JSON file.
-
-        Returns:
-            list: A list of instances created from the data
-            stored in a JSON file.
-        """
+        """ returns a list of instances """
         try:
             with open(cls.__name__ + ".json", "r") as f:
                 contentRead = f.read()
@@ -116,13 +63,7 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
-        """
-        Draw a list of rectangles and squares using the 'turtle' module.
-
-        Args:
-            list_rectangles (list): A list of Rectangle objects to be drawn.
-            list_squares (list): A list of Square objects to be drawn.
-        """
+        """ draw list of rec ands squares """
         for rec in list_rectangles:
             rec = rec.to_dictionary()
             turtle.up()
@@ -136,7 +77,6 @@ class Base:
             turtle.fd(rec["width"])
             turtle.right(90)
             turtle.fd(rec["height"])
-
         for square in list_squares:
             size = square["size"]
             turtle.up()
