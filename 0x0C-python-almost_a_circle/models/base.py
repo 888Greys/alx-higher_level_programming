98b8e41bab230c7ea01f05c3284cd7cc8d65c71a
888Greys <toxicgreys001@gmail.com>
2023-09-21 07:48:23 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Base class """
+""" Base to a  class """
 import json
 import turtle
 
 
 class Base:
-    """ Base class """
+    """ Base to a  class """
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -17,14 +17,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ converts dict to json """
+        """It  converts dictionary to json """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ writes the JSON string representation to file """
+        """it  writes the JSON string representation to file """
         LO_dict = []
         if list_objs is not None:
             LO_dict = [x.to_dictionary() for x in list_objs]
@@ -33,14 +33,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """ returns the list of the JSON string representation """
+        """It  returns the list of the JSON string representation """
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """ create an instance of the class and return it """
+        """It create an instance of the class and return it """
         if cls.__name__ == "Rectangle":
             inst = cls(1, 1)
         elif cls.__name__ == "Square":
@@ -52,7 +52,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """ returns a list of instances """
+        """it returns a list of instances """
         try:
             with open(cls.__name__ + ".json", "r") as f:
                 contentRead = f.read()
@@ -63,7 +63,7 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
-        """ draw list of rec ands squares """
+        """It draw list of rec ands squares """
         for rec in list_rectangles:
             rec = rec.to_dictionary()
             turtle.up()
