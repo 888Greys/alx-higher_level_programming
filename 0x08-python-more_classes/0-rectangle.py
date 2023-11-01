0653f977ba03f5143da97d297b8fd323f22b3425
888Greys <toxicgreys001@gmail.com>
2023-11-01 14:23:28 -0700

    Rec

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
 
-This is an empty class named Rectangle, which serves
-as a blueprint for defining rectangles in a program.
-
-"""
+"""This is an empty class named Rectangle, which serves
+as a blueprint for defining rectangles in a program."""
 
 
 class Rectangle:
-"""
-Class of Rectangle defined
-"""
+    """Represents a rectangle."""
     pass
