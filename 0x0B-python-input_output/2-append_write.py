f0b97e4c8d8d111dfb7490d15da7e0a6c31f30b3
888Greys <toxicgreys001@gmail.com>
2023-11-07 10:28:13 -0800

    Input Output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-A Python program that reads and prints lines from a text file.
-
-Usage:
-    - If 'nb_lines' is non-positive or zero, it reads
-    and prints the entire file.
-    - If 'nb_lines' is positive, it reads and prints the
-    specified number of lines from the file.
-
-Parameters:
-    filename (str): The name of the file to read.
-    nb_lines (int): The number of lines to read and print.
-    If non-positive or zero, the entire file is printed.
-"""
+"""Defines a file-appending function."""
 
 
-def read_lines(filename="", nb_lines=0):
-    if filename == "":
-        return
-    if nb_lines <= 0:
-        with open(filename, "r") as f:
-            print(f.read(), end="")
-    else:
-        with open(filename, "r") as f:
-            head = [next(f) for x in range(nb_lines)]
-        print("".join(head), end="")
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a UTF-8
+    encoded text file.
+
+    Args:
+        filename (str): The name of the file to
+        which the text will be appended.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
