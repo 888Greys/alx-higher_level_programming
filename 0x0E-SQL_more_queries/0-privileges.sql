6033200de8f0378b0084cfde9ff33e104fbdb42b
888Greys <toxicgreys001@gmail.com>
2023-12-14 11:22:00 -0800

    Updates

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 --  script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-
-SHOW GRANTS FOR USER 'user_0d_1'@'localhost';
-SHOW GRANTS FOR USER 'user_0d_2'@'localhost';
+-- for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
