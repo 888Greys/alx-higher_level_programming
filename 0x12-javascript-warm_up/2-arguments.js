b1a7647334c27fda68a1babd95822398fe29b8b7
888Greys <toxicgreys001@gmail.com>
2024-01-08 13:31:40 -0800

    JavaScript WarmUp

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
+//  script that prints a message depending of the number of arguments passed:
+const first = 'No argument';
+const second = 'Argument found';
+const third = 'Arguments found';
 
+const numOfArgs = process.argv.length;
 
-if (process.argv.length === 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
-} else { 
-	console.log('Arguments found');
+if (numOfArgs < 3) {
+  console.log(first);
+} else if (numOfArgs < 4) {
+  console.log(second);
+} else {
+  console.log(third);
 }
-
