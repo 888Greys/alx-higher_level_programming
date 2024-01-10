3fbb9a42bbac78cd021cece9eaca40e46de11b29
888Greys <toxicgreys001@gmail.com>
2024-01-10 12:42:42 -0800

    JavaScript Objects Scopes Closures

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * script that imports an array and computes a new array.
+ */
+
+const list = require('./100-data').list;
+
+console.log(list);
+console.log(list.map((x, i) => x * i));
