323995ae016fe2fefe5bdfc27d7e5e0e61a203ac
888Greys <toxicgreys001@gmail.com>
2024-01-14 12:32:00 -0800

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,8 +15,8 @@ database hbtn_0e_0_usa
 """
 
 if __name__ == '__main__':
-    database_connect = MySQLdb.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    database_connect = MySQLdb.connect(
+            host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     database_cursor = database_connect.cursor()
 
     database_cursor.execute(
