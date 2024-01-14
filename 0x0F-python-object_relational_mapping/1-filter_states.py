2d557ecfad3d1b9a7d633cc107d8a5e744ab3669
888Greys <toxicgreys001@gmail.com>
2024-01-14 12:15:46 -0800

    ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name
+starting with N (upper N) from the
+database hbtn_0e_0_usa
+"""
+
+import MySQLdb as db
+from sys import argv
+
+"""
+script that lists all states with a name
+starting with N (upper N) from the
+database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+    database_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    database_cursor = database_connect.cursor()
+
+    database_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+
+    rows_selected = database_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
