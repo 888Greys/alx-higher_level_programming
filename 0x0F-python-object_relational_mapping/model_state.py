a40ee13be59e0689fb625def3dda8d59bacaf529
888Greys <toxicgreys001@gmail.com>
2024-01-14 13:31:39 -0800

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-script defines a State class and
+This script defines a State class and
 a Base class to work with MySQLAlchemy ORM.
 """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base_1 = declarative_base()
+Base = declarative_base()
 
 
-class State(Base_1):
+class State(Base):
     """State class
 
     Attributes:
@@ -20,5 +20,5 @@ class State(Base_1):
     """
     __tablename__ = 'states'
 
-    id_1 = Column(Integer, primary_key=True)
-    name_1 = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
