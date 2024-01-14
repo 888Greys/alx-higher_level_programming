5a501b4103d1e16e465095704d79debaa0e2eb2b
888Greys <toxicgreys001@gmail.com>
2024-01-14 13:18:59 -0800

    ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- script that lists all State objects
- from the database hbtn_0e_6_usa
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
@@ -15,13 +15,13 @@ if __name__ == "__main__":
     from the database.
     """
 
-    database_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(database_url)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    for any_instance in session.query(State).order_by(State.id):
-        print('{0}: {1}'.format(any_instance.id, any_instance.name))
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
