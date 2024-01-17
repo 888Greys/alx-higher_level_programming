922f4089bbb712191bef2e5d949bd5f137e7c9c5
888Greys <toxicgreys001@gmail.com>
2024-01-17 02:41:28 -0800

    Update

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,4 +1,2 @@
-#!/usr/bin/python3
-str = "Python is a language characterized by its object-oriented programming paradigm, interactive nature, and interpreted execution. It blends impressive capabilities with a highly legible syntax."
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+#!/bin/bash/python3
+print('"The Zen of Python", by Tim Peters')
